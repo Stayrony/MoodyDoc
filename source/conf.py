78d01d63c4fa15cbd550c046bb219788b7d4497d
Stayrony <beyoncem7@gmail.com>
2015-10-29 16:35:25 +0200

    Update conf.py
    change theme to default

--- source/conf.py
+++ source/conf.py
@@ -113,8 +113,8 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
-#html_theme = 'classic'
+html_theme = 'default'
+#html_theme = 'classic' default alabaster
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
